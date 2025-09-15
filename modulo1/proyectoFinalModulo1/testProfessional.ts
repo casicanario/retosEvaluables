154785b05c51aee57e584cf49aee31fb7ee059c7
Fran Marquez <casicanario@gmail.com>
2025-09-15 13:42:54 +0100

    Mantener solo modulo3 en modulo3-tema1

--- modulo1/proyectoFinalModulo1/testProfessional.ts
+++
@@ -1,8 +0,0 @@
-import { Professional } from './professional';
-
-const professional1 = new Professional("Tom Hanks", 65, 80, 1.83, false, "estadounidense", 2, "actor");
-const professional2 = new Professional("Quentin Tarantino", 58, 90, 1.85, false, "estadounidense", 2, "director");
-
-professional1.showProfessionalData();
-console.log("\n"); // Para separar las impresiones
-professional2.showProfessionalData();
