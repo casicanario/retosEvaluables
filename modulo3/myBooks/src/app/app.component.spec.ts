16f9fe56779c68b5eb2787cad9c223318b6e5b66
Fran Marquez <casicanario@gmail.com>
2025-09-18 14:34:15 +0100

    actualizado tema2

--- modulo3/myBooks/src/app/app.component.spec.ts
+++ modulo3/myBooks/src/app/app.component.spec.ts
@@ -14,27 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
-  it(`should have as title 'Mybooks'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Mybooks');
-=======
   it(`should have as title 'myBooks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('myBooks');
->>>>>>> origin/main
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<< HEAD
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Mybooks app is running!');
-=======
     expect(compiled.querySelector('.content span')?.textContent).toContain('myBooks app is running!');
->>>>>>> origin/main
   });
 });
