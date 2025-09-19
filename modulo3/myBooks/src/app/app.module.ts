45cc2d48231ad05ce2dcfe8bdbe1bffc1212c8d3
Fran Marquez <casicanario@gmail.com>
2025-09-19 13:49:30 +0100

    subida reto profile

--- modulo3/myBooks/src/app/app.module.ts
+++ modulo3/myBooks/src/app/app.module.ts
@@ -2,7 +2,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
--- modulo3/myBooks/src/app/components/form-register/form-register.component.ts
+++ modulo3/myBooks/src/app/components/form-register/form-register.component.ts
@@ -1,98 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-register',
   templateUrl: './form-register.component.html',
   styleUrls: ['./form-register.component.css']
 })
-export class FormRegisterComponent implements OnInit {
-  registerForm!: FormGroup;
-  isSubmitting = false;
+export class FormRegisterComponent {
+  registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
-      lastname: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', [Validators.required]]
-    }, { validators: this.passwordMatchValidator });
-  }
-
-  // Validador personalizado para confirmar contraseña
-  passwordMatchValidator(control: AbstractControl): {[key: string]: any} | null {
-    const password = control.get('password');
-    const confirmPassword = control.get('confirmPassword');
-    
-    if (password && confirmPassword && password.value !== confirmPassword.value) {
-      return { passwordMismatch: true };
-    }
-    return null;
-  }
-
-  onSubmit(): void {
-    if (this.registerForm.valid) {
-      this.isSubmitting = true;
-      const formData = this.registerForm.value;
-      console.log('Datos de registro:', formData);
-      
-      // Simular proceso de registro
-      setTimeout(() => {
-        alert('¡Registro exitoso! Bienvenido a MyBooks');
-        this.registerForm.reset();
-        this.isSubmitting = false;
-      }, 2000);
-    } else {
-      this.markFormGroupTouched();
-    }
-  }
-
-  private markFormGroupTouched(): void {
-    Object.keys(this.registerForm.controls).forEach(key => {
-      const control = this.registerForm.get(key);
-      control?.markAsTouched();
+    }, {
+      validators: this.passwordMatchValidator
     });
   }
 
-  getFieldError(fieldName: string): string {
-    const field = this.registerForm.get(fieldName);
-    if (field?.errors && field.touched) {
-      if (field.errors['required']) {
-        return `El campo ${this.getFieldDisplayName(fieldName)} es requerido`;
-      }
-      if (field.errors['email']) {
-        return 'El email no tiene un formato válido';
-      }
-      if (field.errors['minlength']) {
-        return `El campo debe tener al menos ${field.errors['minlength'].requiredLength} caracteres`;
-      }
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+    
+    if (password && confirmPassword && password.value !== confirmPassword.value) {
+      confirmPassword.setErrors({ passwordMismatch: true });
+    } else {
+      confirmPassword?.setErrors(null);
     }
-
-    // Validación especial para confirmar contraseña
-    if (fieldName === 'confirmPassword' && this.registerForm.errors?.['passwordMismatch'] && field?.touched) {
-      return 'Las contraseñas no coinciden';
-    }
-
-    return '';
   }
 
-  private getFieldDisplayName(fieldName: string): string {
-    const names: {[key: string]: string} = {
-      name: 'nombre',
-      lastname: 'apellido',
-      email: 'email',
-      password: 'contraseña',
-      confirmPassword: 'confirmar contraseña'
-    };
-    return names[fieldName] || fieldName;
-  }
-
-  hasFieldError(fieldName: string): boolean {
-    const field = this.registerForm.get(fieldName);
-    return !!(field?.errors && field.touched) || 
-           (fieldName === 'confirmPassword' && this.registerForm.errors?.['passwordMismatch'] && field?.touched);
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulario válido:', this.registerForm.value);
+      // Aquí irá la lógica para enviar los datos al servidor
+      alert('Registro exitoso!');
+      this.registerForm.reset();
+    } else {
+      Object.keys(this.registerForm.controls).forEach(key => {
+        const control = this.registerForm.get(key);
+        if (control?.invalid) {
+          control.markAsTouched();
+        }
+      });
+    }
   }
 }
